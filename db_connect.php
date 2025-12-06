a7be5e3a33f8a51feaa135ef53170f187d172022
Hacktor <143013613+alsharhy@users.noreply.github.com>
2025-12-06 03:05:02 +0300

    Change database connection parameters to localhost

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('sql105.infinityfree.com', 'if0_39887797', 'gOLTY2PxODPH246', 'if0_39887797_Profile');
+$conn = mysqli_connect('localhost', 'username', 'password', 'dbname');
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
